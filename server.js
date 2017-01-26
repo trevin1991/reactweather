a6892054f2f720137596b011ef5d4c3c625ca014
Eugene Trofimovich <trofimovich.eugene.projects@gmail.com>
2017-01-26 11:29:13 +0300

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 'use strict';
 let express = require('express');
 
-const PORT = 3000;
-
-// Create our app
+//const PORT = 3000;
 let app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+    if(req.headers["x-forwared-proto"] === "http") {
+        next();
+    } else {
+        res.redirect("http://" + req.hostname + req.url);
+    }
+});
+// Create our app
 app.use(express.static('public'));
 app.listen(PORT, () => console.log(`Express server is up on port ${PORT}`));
