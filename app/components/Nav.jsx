df49fde317fd2307e2ddd9fbad56eaa77e0948f4
Eugene Trofimovich <trofimovich.eugene.projects@gmail.com>
2017-01-27 09:28:58 +0300

    Styling navbar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -2,14 +2,31 @@ let React = require("react"),
     {Link, IndexLink} = require("react-router");
 
 class Nav extends React.Component {
+    onSearch(e) {
+        e.preventDefault();
+        console.warn("Not yet implemented");
+    }
+
     render() {
         return (
-            <div>
-                <h2>Nav Component</h2>
-                <IndexLink to="/" activeClassName="active">Get Weather</IndexLink>
-                <Link to="/favourites" activeClassName="active">Favourites</Link>
-                <Link to="/about" activeClassName="active">About</Link>
-                <Link to="/examples" activeClassName="active">Examples</Link>
+            <div className="top-bar">
+                <div className="top-bar-left">
+                    <ul className="menu">
+                        <li className="menu-text">Weather App</li>
+                        <li><IndexLink to="/" activeClassName="active">Get Weather</IndexLink></li>
+                        <li><Link to="/favourites" activeClassName="active">Favourites</Link></li>
+                        <li><Link to="/about" activeClassName="active">About</Link></li>
+                        <li><Link to="/examples" activeClassName="active">Examples</Link></li>
+                    </ul>
+                </div>
+                <div className="top-bar-right">
+                    <form onSubmit={ this.onSearch.bind(this) }>
+                        <ul className="menu">
+                            <li><input type="search" placeholder="Enter the place"/></li>
+                            <li><button type="submit" className="button">Get Weather</button></li>
+                        </ul>
+                    </form>
+                </div>
             </div>
         )
     }
